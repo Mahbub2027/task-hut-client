ae8909c9ed33c64818fd609e4e299dc94d964ece
tryit-100 <safwan.std21@gmail.com>
2024-01-29 12:24:54 +0600

    jobPost and employe card is added

---
+++ src/Pages/FindJobs/FindJobs.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import JobPostCard from './JobPostCard/JobPostCard';
+
+const FindJobs = () => {
+    return (
+        <div className='w-full'>
+            Find jobs
+            <JobPostCard/>
+        </div>
+    );
+};
+
+export default FindJobs;
