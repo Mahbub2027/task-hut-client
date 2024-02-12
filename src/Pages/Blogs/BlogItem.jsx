b162b4e5aa1219003e952b034a6cf2fa0711fb9c
tryit-100 <safwan.std21@gmail.com>
2024-02-12 23:00:48 +0600

    blogs,blogsCard and blogs details UI updated

--- src/Pages/Blogs/BlogItem.jsx
+++ src/Pages/Blogs/BlogItem.jsx
@@ -1,30 +1,33 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { format } from "timeago.js";
 
-const BlogItem = ({item}) => {
+const BlogItem = ({ item }) => {
 
-  const id = item?._id;
-  let shortDesc;
-  if (item.description) {
-    shortDesc =
-      item?.description.length > 150 ? item?.description.substr(0, 150) + "..." : item?.description;
-  }
-  console.log("id: " + id);
+  const { _id, title, img, description, bloggerImg, bloggerName, createdAt } = item;
+  // const id = item?._id;
+  // console.log(item)
 
   return (
-    <div className="card w-96 glass rounded-3xl shadow-2xl ">
-      <figure>
-        <img src={item?.img} alt="" className="max-h-48 w-full object-cover" />
-      </figure>
-      <div className="card-body bg-stone-200 rounded-b-3xl ">
-        <h2 className="card-title text-gray-800">{item?.title}</h2>
-        <p className="text-sm text-slate-600">{shortDesc}</p>
-        <div className="card-actions justify-end">
-          <NavLink to={`/blogs/${id}`}>
-            {" "}
-            <button className="border-white border-solid  border-2 text-gray-700 font-semibold bg-orange-50 rounded-3xl w-24 h-10 hover:bg-stone-300">
-              View
-            </button>{" "}
+    <div className="rounded-3xl shadow-md bg-white h-[400px] border">
+      <div className="w-full h-1/2">
+        <img src={img} alt="" className="w-full h-full object-fill rounded-t-3xl" />
+      </div>
+      <div className="h-1/2 rounded-b-3xl px-4 py-2 text-slate-700 flex flex-col justify-between">
+        <div>
+          <small className="text-sm text-slate-400">{format(createdAt)}</small>
+          <h2 className="text-xl font-semibold">{title}</h2>
+          <p className="text-sm text-slate-500">{description.split(' ').slice(0, 10).join(' ')}</p>
+        </div>
+        <div className="flex justify-between">
+          <div className="flex items-center gap-2">
+            <img className="w-10 h-10 rounded-full" src={bloggerImg} alt="" />
+            <h3 className="font-medium">{bloggerName}</h3>
+          </div>
+          <NavLink to={`/blogs/${_id}`}>
+            <button className="m-1 shadow-lg border-2 border-indigo-800 rounded-full bg-indigo-800 px-3 py-1 text-white text-sm hover:bg-indigo-500 hover:border-indigo-500 transition-all ease-out delay-0 duration-500">
+              Read more
+            </button>
           </NavLink>
         </div>
       </div>
--- src/Pages/Blogs/SingleBlog.jsx
+++ src/Pages/Blogs/SingleBlog.jsx
@@ -19,32 +19,28 @@ const SingleBlog = () => {
   }, []);
 
   return (
-    <div className="w-full bg-stone-200">
+    <>
       <div className="w-9/12 mx-auto">
-        <div className="p-20 lg:p-20 lg:pr-0">
+        <div className="bg-slate-100 p-4">
           <img
             className="w-full h-[500px] rounded-2xl object-cover shadow-2xl"
             src={blog.img}
             alt=""
           />
-          <h1 className="text-center mt-10 text-gray-800 font-semibold mb-5 text-3xl ">{blog.title}</h1>
-          <div className="flex justify-between mb-5 text-lg font-semibold font-mono text-violet-500">
-            <span>
-              Author:
-              <b className="ml-1">
-                <Link className="link" to="/profile?username=mahfuzuser">
-                  {blog.bloggerName}
-                </Link>
-              </b>
-            </span>
+          <div className="flex justify-between my-5 text-lg font-semibold font-mono text-slate-400">
+            <div className="flex items-center gap-2">
+              <img className="w-10 h-10 rounded-full" src={blog.bloggerImg} alt="" />
+              <h3 className="font-medium text-slate-700">{blog.bloggerName}</h3>
+            </div>
             <span>{format(blog.createdAt)}</span>
           </div>
-          <p className="text-lg first-letter:text-2xl first-letter:font-bold text-stone-600">
+          <h1 className="text-slate-700 font-bold my-5 text-3xl">{blog.title}</h1>
+          <p className="text-lg first-letter:text-2xl first-letter:font-bold text-slate-500 bg-white p-2 border-8 border-white rounded-2xl">
             {blog.description}
           </p>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
