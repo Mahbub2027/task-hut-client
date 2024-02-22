a4ec56b96ff1db03aa68512d3d36b953f4b2bddb
Mahbub2027 <alam201127@gmail.com>
2024-02-22 15:23:00 +0600

    employees details updated

--- src/Pages/FindEmployee/EmployeeDetails/EmployeeDetails.jsx
+++ src/Pages/FindEmployee/EmployeeDetails/EmployeeDetails.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
 import { FaLocationDot } from 'react-icons/fa6';
 import { useLoaderData } from 'react-router-dom';
+import useAuth from '../../../hooks/useAuth';
+import { useQuery } from '@tanstack/react-query';
+import useAxiosPublic from '../../../hooks/useAxiosPublic';
 
 const EmployeeDetails = () => {
+    const {user} =  useAuth();
+    const axiosPublic  = useAxiosPublic();
     const {_id, name,employee_email,date_birth ,cover_img,linkedin,location,city,country,
         profession,experience,workPreference,resume,portfolio,github ,skills ,about,} = useLoaderData();
+    
+        const { data: users = [] } = useQuery({
+            queryKey: ['users'],
+            queryFn: async () => {
+                const res = await axiosPublic.get(`/users`)
+                return res.data;
+            }
+        })
+    
     return (
         <div className='w-11/12 mx-auto my-10'>
-            <span><img className='w-40 h-40 rounded-lg' src={cover_img} alt="" /></span>
+            <span><img className='w-full h-60 rounded-lg' src={cover_img} alt="" /></span>
+            
+            {
+                users.map(use=> <div key={use._id}>
+                    {
+                        use.email === employee_email && <>
+                        <span><img className='w-24 h-24 rounded-full' src={use?.image} alt="" /></span>
+                        </>
+                    }
+                </div>)
+            }
+            {/* <span><img className='w-28 h-28 rounded-full' src={user?.photoURL} alt="" /></span> */}
+            
             <h2 className='text-2xl font-bold'>{name}</h2>
             <p>{profession}</p>
             <div >
@@ -17,6 +43,7 @@ const EmployeeDetails = () => {
             </div>
             <p>Email: {employee_email}</p>
             <p>DOB: {date_birth}</p>
+            <p>Work Preference: {workPreference}</p>
             <p>Experience: {experience}</p>
             <p>About: {about}</p>
             <p>Portfoli: {portfolio}</p>
