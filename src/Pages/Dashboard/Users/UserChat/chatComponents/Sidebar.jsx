6ef85f34b34fd086c98eeef3b50fb1d6c2248134
tryit-100 <safwan.std21@gmail.com>
2024-02-25 16:17:09 +0600

    userChat UI updated

--- src/Pages/Dashboard/Users/UserChat/chatComponents/Sidebar.jsx
+++ src/Pages/Dashboard/Users/UserChat/chatComponents/Sidebar.jsx
@@ -3,8 +3,7 @@ import SideChats from './SideChats';
 
 const Sidebar = () => {
     return (
-        <div className='w-1/3 md:w-5/12 lg:w-2/5 bg-slate-500 rounded-l-xl overflow-hidden'>
-    
+        <div className='w-1/3 md:w-5/12 lg:w-2/5 bg-slate-50 rounded-l-xl overflow-hidden'>
         <SideChats/>
     </div>
     );
