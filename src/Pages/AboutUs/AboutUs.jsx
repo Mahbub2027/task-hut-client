9851f29c85df7fb4f5d9ced34b30a772d6551953
Abid Rahman Shafi <abidurrahman780@gmail.com>
2024-03-09 04:38:58 +0600

    Carrer Page, Newsletter Design

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -2,7 +2,6 @@ import { Helmet } from "react-helmet";
 import logo from "../../../public/TaskhutClear.png";
 import developer from "../../assets/images/developer.jpg";
 import WhyTaskHut from "./WhyTaskHut";
-import Statistics from "./Statistics";
 import { Typewriter } from "react-simple-typewriter";
 
 import AOS from "aos";
@@ -10,11 +9,8 @@ import "aos/dist/aos.css";
 import { Link } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
 
-
-
-const  AboutUs = () => {
-
-  const {user} = useAuth();
+const AboutUs = () => {
+  const { user } = useAuth();
   AOS.init();
 
   return (
@@ -27,25 +23,26 @@ const  AboutUs = () => {
         About Us -<span className="text-black"> Task</span>
         <span className="text-indigo-600">Hut</span>
       </h1>
-      
 
       <div className="text-black text-center">
         <h3 className="mb-5 text-2xl font-bold bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent">
-          <span className="text-slate-700 text-3xl block">Empowering Global Talent</span>
+          <span className="text-slate-700 text-3xl block">
+            Empowering Global Talent
+          </span>
           <br />{" "}
           <Typewriter
             words={["Unveiling TaskHut`s Mission in the Digital Workspace"]}
             loop={false}
             cursor
             cursorStyle="_"
-            typeSpeed={70}
+            typeSpeed={100}
             deleteSpeed={50}
             delaySpeed={1000}
           />
         </h3>
       </div>
 
-      <div className="lg:flex items-center justify-evenly p-20 bg-purple-200">
+      <div className="lg:flex items-center justify-evenly p-20 bg-indigo-200">
         <div data-aos="fade-right" data-aos-duration="500">
           <img src={logo} alt="" />
         </div>
@@ -53,7 +50,7 @@ const  AboutUs = () => {
         <div
           data-aos="zoom-out-up"
           data-aos-duration="1000"
-          className="card w-96 bg-purple-700 text-primary-content items-center"
+          className="card w-96 bg-indigo-700 text-primary-content items-center"
         >
           <div className="card-body">
             <p className="p-8 text-wrap text-white text-justify">
@@ -66,7 +63,9 @@ const  AboutUs = () => {
             </p>
             <div className="card-actions justify-end">
               <Link to="/login">
-                <button className="btn font-bold">Join TaskHut</button>
+                {!user?.emailVerified && (
+                  <button className="primary-btn">Join TaskHut</button>
+                )}
               </Link>
             </div>
           </div>
@@ -78,7 +77,7 @@ const  AboutUs = () => {
         <div
           data-aos="zoom-out-up"
           data-aos-duration="1000"
-          className="bg-purple-200 rounded sm:w-auto"
+          className="bg-indigo-200 rounded sm:w-auto"
         >
           <p className=" lg:text-xl text-black text-justify p-10">
             TaskHut was born out of a vision to create a global marketplace that
@@ -172,11 +171,9 @@ const  AboutUs = () => {
         className="card-actions justify-center p-10"
       >
         <Link to="/login">
-          {
-            !user?.emailVerified && <button className="primary-btn">
-            Join TaskHut
-          </button> 
-          }
+          {!user?.emailVerified && (
+            <button className="primary-btn">Join TaskHut</button>
+          )}
         </Link>
       </div>
 
@@ -193,8 +190,60 @@ const  AboutUs = () => {
       <div className="lg:flex items-center justify-center p-20">
         {/* <Statistics></Statistics> */}
       </div>
+
+
+      <section className="mb-20">
+        <h1 className="text-center drop-shadow-2xl text-purple-600 font-extrabold text-4xl">
+          Our Brand Partners
+        </h1>
+        <div className="bg-white py-12 sm:py-6">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <h2 className="text-center text-lg mb-10 font-semibold leading-8 text-gray-900">
+              Trusted by the world`s most innovative teams
+            </h2>
+            <div className="mx-auto grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+              <img
+                className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
+                alt="Transistor"
+                width={158}
+                height={48}
+              />
+              <img
+                className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
+                alt="Reform"
+                width={158}
+                height={48}
+              />
+              <img
+                className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
+                alt="Tuple"
+                width={158}
+                height={48}
+              />
+              <img
+                className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+                src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
+                alt="SavvyCal"
+                width={158}
+                height={48}
+              />
+              <img
+                className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+                src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
+                alt="Statamic"
+                width={158}
+                height={48}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
     </div>
   );
-}
+};
 
 export default AboutUs;
